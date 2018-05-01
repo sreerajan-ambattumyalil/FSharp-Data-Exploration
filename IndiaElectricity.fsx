9d0412f1feffbe6f350f4a36203aae875727ed26
Sreerajan Ambattumyalil <sreerajan.a@gmail.com>
2018-05-01 20:51:10 +0530

    Scatter plot for electrification in India

--- IndiaElectricity.fsx
+++ IndiaElectricity.fsx
@@ -1,17 +1,35 @@
-#load "packages/FsLab/Themes/DefaultWhite.fsx"
+#load "packages/FsLab/Themes/AtomChester.fsx"
 #load "packages/FsLab/FsLab.fsx"
 
+open System
 open Deedle
 open FSharp.Data
-open XPlot.GoogleCharts
-open XPlot.GoogleCharts.Deedle
+open XPlot.Plotly
 let wb = WorldBankData.GetDataContext()
 let india = wb.Countries.India.Indicators
 
 let inRuralElectricity = series india.``Access to electricity, rural (% of rural population)``
 let inUrbanElectricity = series india.``Access to electricity, urban (% of urban population)``
 
-[inRuralElectricity.[1947 .. 2016]; inUrbanElectricity.[1947 .. 2016];]
-|> Chart.Line
-|> Chart.WithOptions (Options(legend = Legend(position = "bottom")))
+let ruralTrace  =
+    Scatter(
+        x = inRuralElectricity.[1947 .. 2016].Keys,
+        y =inRuralElectricity.[1947 .. 2016].Values
+    )
+
+let urbanTrace =
+    Scatter(
+        x = inUrbanElectricity.[1947 .. 2016].Keys,
+        y = inUrbanElectricity.[1947 .. 2016].Values
+    )
+
+[ruralTrace; urbanTrace]
+|> Chart.Plot
+|> Chart.WithWidth 700
+|> Chart.WithHeight 500
+|> Chart.WithXTitle "Year"
+|> Chart.WithYTitle "Electrification (%)"
+|> Chart.WithTitle "Electrification in India"
 |> Chart.WithLabels ["Rural"; "Urban"]
+|> Chart.Show
+
